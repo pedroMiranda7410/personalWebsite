24a7808aafa3582f68987b29b5426fe009424855
Pedro Henrique Queiroz Miranda <pmiranda7410@gmail.com>
2019-07-09 21:51:31 -0300

    Changing app.css to app.scss

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,2 +1,19 @@
 class ContactsController < ApplicationController
+  require 'sendgrid-ruby'
+  include SendGrid
+
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(params[:contact])
+    @contact.request = request
+    if @contact.deliver
+      flash.now[:error] = nil
+    else
+      flash.now[:error] = 'Não pode enviar a mensagem.'
+      render :new
+    end
+  end
 end

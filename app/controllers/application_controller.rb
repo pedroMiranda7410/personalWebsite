0ea92c546c63a7a3c4757b5a4fa5610f0dbf866d
Pedro Henrique Queiroz Miranda <pmiranda7410@gmail.com>
2019-08-16 09:38:42 -0300

    Desenvolvendo página de login e arrumando alguns bugs com navbar, botao e colunas

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,33 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+
+  add_flash_types :danger, :success, :info, :warning
+
+  def new
+
+    if User.all == []
+      @admin = User.new(password:"123",name: "Pedro Miranda", admin: true, nickname:"pedro.jams", email:"pmiranda7410@gmail.com", avatar: "")
+      @admin.save
+    end
+
+    session[:user_id] = nil
+  end
+
+  def current_user
+
+    if session[:user_id]
+      @current_user ||= User.find(session[:user_id])
+    else
+      @current_user = nil
+    end
+
+  end
+
+
+  def authenticate
+
+      redirect_to(login_path) unless current_user
+
+  end
+
 end
---
+++ test/models/add_employees_to_attachment_avatar_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class AddEmployeesToAttachmentAvatarTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
